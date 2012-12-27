2aac5e2acf7cb41ab1cae9d776bf7a0c6fd61afb
Matt Bolt <mbolt35@gmail.com>
2012-12-27 11:39:49 -0500

    Fix LockingEnumerator implementation
    Correctly implement IEnumerator

--- CSharp/Concurrent/LockingEnumerator.cs
+++ CSharp/Concurrent/LockingEnumerator.cs
@@ -60,5 +60,10 @@ namespace CSharp.Concurrent {
             }
         }
 
+        object IEnumerator.Current {
+            get {
+                return Current;
+            }
+        }
     }
 }
