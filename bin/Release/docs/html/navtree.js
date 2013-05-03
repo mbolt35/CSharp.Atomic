dfa1f6a61b22c0d9bff380eb3628d1599f66f3d8
Matt Bolt <matt@electrotank.com>
2013-05-02 21:31:16 -0400

    Adding docs and script temporarily

---
+++ bin/Release/docs/html/navtree.js
@@ -0,0 +1,301 @@
+var NAVTREE =
+[
+  [ "CSharp.Atomic: Concurrent Data Structures", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "CSharp.Atomic.Atomic< T >", "d9/d3c/class_c_sharp_1_1_atomic_1_1_atomic-g.html", null ],
+      [ "CSharp.Atomic.AtomicBoolean", "d1/da3/class_c_sharp_1_1_atomic_1_1_atomic_boolean.html", null ],
+      [ "CSharp.Atomic.AtomicInt", "d0/d38/class_c_sharp_1_1_atomic_1_1_atomic_int.html", null ],
+      [ "CSharp.Atomic.AtomicLong", "d1/d16/class_c_sharp_1_1_atomic_1_1_atomic_long.html", null ],
+      [ "CSharp.Collections.Concurrent.BlockingQueue< T >", "d5/d11/class_c_sharp_1_1_collections_1_1_concurrent_1_1_blocking_queue-g.html", null ],
+      [ "CSharp.Locking.ConditionIds", "d1/df1/class_c_sharp_1_1_locking_1_1_condition_ids.html", null ],
+      [ "CSharp.Threading.CountDownLatch", "d8/d54/class_c_sharp_1_1_threading_1_1_count_down_latch.html", null ],
+      [ "CSharp.Collections.HashStack< T, U >", "da/de9/class_c_sharp_1_1_collections_1_1_hash_stack-g.html", null ],
+      [ "CSharp.Locking.ICondition", "db/d75/interface_c_sharp_1_1_locking_1_1_i_condition.html", null ],
+      [ "CSharp.Locking.ILock", "d8/d01/interface_c_sharp_1_1_locking_1_1_i_lock.html", null ],
+      [ "CSharp.Locking.LockCondition", "de/d03/class_c_sharp_1_1_locking_1_1_lock_condition.html", null ],
+      [ "CSharp.Collections.Concurrent.LockingEnumerator< T >", "d3/d29/class_c_sharp_1_1_collections_1_1_concurrent_1_1_locking_enumerator-g.html", null ],
+      [ "CSharp.Locking.LockMonitor", "dc/d29/class_c_sharp_1_1_locking_1_1_lock_monitor.html", null ],
+      [ "CSharp.Locking.ReentrantLock", "d5/d8c/class_c_sharp_1_1_locking_1_1_reentrant_lock.html", null ],
+      [ "CSharp.Threading.SingleThreadExecutor", "d4/d75/class_c_sharp_1_1_threading_1_1_single_thread_executor.html", null ],
+      [ "CSharp.Threading.ThreadLocal< T >", "d2/d9d/class_c_sharp_1_1_threading_1_1_thread_local-g.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "CSharp.Atomic.Atomic< T >", "d9/d3c/class_c_sharp_1_1_atomic_1_1_atomic-g.html", null ],
+      [ "CSharp.Atomic.AtomicBoolean", "d1/da3/class_c_sharp_1_1_atomic_1_1_atomic_boolean.html", null ],
+      [ "CSharp.Atomic.AtomicInt", "d0/d38/class_c_sharp_1_1_atomic_1_1_atomic_int.html", null ],
+      [ "CSharp.Atomic.AtomicLong", "d1/d16/class_c_sharp_1_1_atomic_1_1_atomic_long.html", null ],
+      [ "CSharp.Collections.Concurrent.BlockingQueue< T >", "d5/d11/class_c_sharp_1_1_collections_1_1_concurrent_1_1_blocking_queue-g.html", null ],
+      [ "CSharp.Locking.ConditionIds", "d1/df1/class_c_sharp_1_1_locking_1_1_condition_ids.html", null ],
+      [ "CSharp.Threading.CountDownLatch", "d8/d54/class_c_sharp_1_1_threading_1_1_count_down_latch.html", null ],
+      [ "CSharp.Collections.HashStack< T, U >", "da/de9/class_c_sharp_1_1_collections_1_1_hash_stack-g.html", null ],
+      [ "CSharp.Locking.ICondition", "db/d75/interface_c_sharp_1_1_locking_1_1_i_condition.html", [
+        [ "CSharp.Locking.LockCondition", "de/d03/class_c_sharp_1_1_locking_1_1_lock_condition.html", null ]
+      ] ],
+      [ "CSharp.Locking.ILock", "d8/d01/interface_c_sharp_1_1_locking_1_1_i_lock.html", [
+        [ "CSharp.Locking.ReentrantLock", "d5/d8c/class_c_sharp_1_1_locking_1_1_reentrant_lock.html", null ]
+      ] ],
+      [ "CSharp.Collections.Concurrent.LockingEnumerator< T >", "d3/d29/class_c_sharp_1_1_collections_1_1_concurrent_1_1_locking_enumerator-g.html", null ],
+      [ "CSharp.Locking.LockMonitor", "dc/d29/class_c_sharp_1_1_locking_1_1_lock_monitor.html", null ],
+      [ "CSharp.Threading.SingleThreadExecutor", "d4/d75/class_c_sharp_1_1_threading_1_1_single_thread_executor.html", null ],
+      [ "CSharp.Threading.ThreadLocal< T >", "d2/d9d/class_c_sharp_1_1_threading_1_1_thread_local-g.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Packages", "namespaces.html", [
+      [ "CSharp", "d4/d9e/namespace_c_sharp.html", null ],
+      [ "CSharp.Atomic", "df/d5c/namespace_c_sharp_1_1_atomic.html", null ],
+      [ "CSharp.Collections", "d0/d31/namespace_c_sharp_1_1_collections.html", null ],
+      [ "CSharp.Collections.Concurrent", "d0/d4c/namespace_c_sharp_1_1_collections_1_1_concurrent.html", null ],
+      [ "CSharp.Locking", "d4/d70/namespace_c_sharp_1_1_locking.html", null ],
+      [ "CSharp.Threading", "d4/d27/namespace_c_sharp_1_1_threading.html", null ],
+      [ "CSharp.Util", "d5/d75/namespace_c_sharp_1_1_util.html", null ]
+    ] ],
+    [ "Package Functions", "namespacemembers.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
